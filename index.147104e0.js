ca33a5f78285f1c7023d2ad80a43f9877b034932
Vitalii-Prokopenko <115025319+Vitalii-Prokopenko@users.noreply.github.com>
2023-02-04 16:39:22 +0000

    Deploying to gh-pages from @ Vitalii-Prokopenko/my-ice-cream@d5bb328e1614d8385a9cb5219b951a11c7454a8d 🚀

---
+++ index.147104e0.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,centeredSlides:!0,effect:"slide",grabCursor:!0,speed:1e3,breakpoints:{320:{spaceBetween:10},480:{spaceBetween:10},768:{spaceBetween:15},1200:{spaceBetween:20}},autoplay:{delay:4e3,disableOnInteraction:!1},pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.147104e0.js.map
--- index.ab8c0e9f.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,centeredSlides:!0,effect:"slide",grabCursor:!0,speed:1e3,autoplay:{delay:4e3,disableOnInteraction:!1},pagination:{el:".swiper-pagination"}});
-//# sourceMappingURL=index.ab8c0e9f.js.map
