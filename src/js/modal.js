a4b8fe0eab74d2c76cd06d9c75216bd32bc31f22
Vitalii Prokopenko <doc8259@gmail.com>
2023-08-12 16:10:51 +0300

    refactors open and close modals

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,62 +1,32 @@
-import {
-  enableBodyScroll,
-  disableBodyScroll,
-} from 'body-scroll-lock';
+// BodyScrollLock
+import { enableBodyScroll, disableBodyScroll } from 'body-scroll-lock';
 
-let targetElement;
+// Script for modals - https://medium.com/@dan.postnov/%D0%BC%D0%BE%D0%B4%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BE%D0%BA%D0%BD%D0%B0-%D0%BD%D0%B0-javascript-30-%D1%81%D1%82%D1%80%D0%BE%D0%BA-%D0%BA%D0%BE%D0%B4%D0%B0-dbbb599649f3
+// Closest is replaced by data-modal identification
 
-!(function (e) {
-  'function' != typeof e.matches &&
-    (e.matches =
-      e.msMatchesSelector ||
-      e.mozMatchesSelector ||
-      e.webkitMatchesSelector ||
-      function (e) {
-        for (
-          var t = this,
-            o = (t.document || t.ownerDocument).querySelectorAll(e),
-            n = 0;
-          o[n] && o[n] !== t;
-
-        )
-          ++n;
-        return Boolean(o[n]);
-      }),
-    'function' != typeof e.closest &&
-      (e.closest = function (e) {
-        for (var t = this; t && 1 === t.nodeType; ) {
-          if (t.matches(e)) return t;
-          t = t.parentNode;
-        }
-        return null;
-      });
-})(window.Element.prototype);
-
+// DOMContentLoaded - when when the HTML document has been completely parsed,
+//  and all deferred scripts (<script defer src="…"> and <script type="module">)
+//  have downloaded and executed
 document.addEventListener('DOMContentLoaded', function () {
-  /* Записываем в переменные массив элементов-кнопок и подложку.
-      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
-  var modalButtons = document.querySelectorAll('.js-open-modal'),
-    overlay = document.querySelector('.js-overlay-modal'),
-    closeButtons = document.querySelectorAll('.js-modal-close');
+  /* Write an array of button elements and a background to variables */
+  const modalButtons = document.querySelectorAll('.js-open-modal');
+  const overlay = document.querySelector('.js-overlay-modal');
+  const closeButtons = document.querySelectorAll('.js-modal-close');
 
-  /* Перебираем массив кнопок */
+  /* Loop through an array of buttons */
   modalButtons.forEach(function (item) {
-    /* Назначаем каждой кнопке обработчик клика */
+    /* Assign a click handler to each button */
     item.addEventListener('click', function (e) {
-      /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
       e.preventDefault();
+      /* Each time the button is clicked, we will fetch the contents of the data-modal attribute and
+      look for a modal window with the same attribute */
+      const modalId = this.getAttribute('data-modal');
+      const modalElem = document.querySelector(
+        '.modal[data-modal="' + modalId + '"]'
+      );
 
-      /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
-      var modalId = this.getAttribute('data-modal'),
-        modalElem = document.querySelector(
-          '.modal[data-modal="' + modalId + '"]'
-        );
-
-      /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
+      /* After we have found the desired modal window, add classes
+      for overlay and modal window to show them */
       modalElem.classList.add('active');
       overlay.classList.add('active');
 
@@ -65,25 +35,35 @@ document.addEventListener('DOMContentLoaded', function () {
     }); // end click
   }); // end foreach
 
+  // Close modal windows
+
   closeButtons.forEach(function (item) {
     item.addEventListener('click', function (e) {
-      var parentModal = this.closest('.modal');
+      // const parentModal = this.closest('.modal');
+
+      const modalIdClose = this.getAttribute('data-modal');
+      const parentModal = document.querySelector(
+        '.modal[data-modal="' + modalIdClose + '"]'
+      );
 
       parentModal.classList.remove('active');
       overlay.classList.remove('active');
 
       // enables body scroll when modal window is closed
-        enableBodyScroll(parentModal);
+      enableBodyScroll(parentModal);
       // Stops video from youtube when modal is closed
-      var videoEmbed = document.querySelector('.modal-about__video');
-      videoEmbed.setAttribute('src', "")
+
+      if (modalIdClose === '2') {
+        const videoEmbed = document.querySelector('.modal-about__video');
+        videoEmbed.setAttribute('src', '');
+      }
     });
   }); // end foreach
 
   document.body.addEventListener(
-    'keyup',
+    'keydown',
     function (e) {
-      var key = e.keyCode;
+      const key = e.code;
 
       if (key == 27) {
         document.querySelector('.modal.active').classList.remove('active');
